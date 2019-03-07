94df439e56d6f3db7a62bf569309fc77087220ef
Nigel Deakin <nigel.deakin@oracle.com>
2019-03-07 16:36:29 +0000

    Fixit 2

--- util/signal.go
+++ util/signal.go
@@ -51,7 +51,7 @@ func (s *SignalMonkey) Add(fn *SignalHandler) {
 
 // Remove a handler from our array
 func (s *SignalMonkey) Remove(fn *SignalHandler) {
-	util.RootLogger().WithField("Logger", "Remove").Errorln("Removing SignalHandler:" + fn.ID
+	util.RootLogger().WithField("Logger", "Remove").Errorln("Removing SignalHandler:" + fn.ID)
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 	for i, x := range s.handlers {
